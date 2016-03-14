5ac6a5e60d6c659f3fe27c0d4938277de97a4241
Alex McArther <acmcarther@gmail.com>
2016-03-14 10:58:33 -0700

    Midcommit

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-#![feature(custom_derive, plugin)]
+#![feature(custom_derive, plugin, type_ascription)]
 #![plugin(serde_macros)]
 
 extern crate regex;
@@ -14,6 +14,58 @@ pub mod helpers;
 pub use cucumber::{ Step, Cucumber, CucumberRegistrar, InvokeResponse, InvokeArgument, FailMessage};
 pub use runner::{ WorldRunner };
 pub use server::{ Server };
+
+/* Note to me
+ * The below facilitates auto step destructuring, consider expanding later 
+ *
+ *
+ *
+ */
+
+#[macro_export]
+macro_rules! destructured_with_cuke {
+  ([$($i:ident: $t:ty),*], $e: expr) => {
+    Box::new(|cuke: CucumberState, state: State, args: Vec<InvokeArgument>| {
+      $(
+      let $i = "hello".to_owned();
+      )*
+      $e(cuke, state, $($i),*)
+    })
+  }
+}
+
+#[macro_export]
+macro_rules! destructured {
+  ([$($i:ident: $t:ty),*], $e: expr) => {
+    Box::new(|_: CucumberState, state: State, args: Vec<InvokeArgument>| {
+      $(
+      let $i = "hello".to_owned();
+      )*
+      $e(state, $($i),*)
+    })
+  }
+}
+
+
+#[cfg(test)]
+mod test{
+  pub use cucumber::InvokeArgument;
+  pub use cucumber::CucumberState;
+
+  type State = u32;
+
+  #[test]
+  fn whatever() {
+    let shit = ("sheeeit".to_owned() + (1 + 1 + 2 + 1.5 ): &str): String;
+    destructured_with_cuke!( [a: String, b: String], |cuke: CucumberState, state: State, a: String, b: String| {
+      println!("{}", state);
+    });
+    destructured!( [a: String, b: String], |state: State, a: String, b: String| {
+      println!("{}", state);
+    });
+  }
+}
+
 
 #[macro_export]
 macro_rules! cuke_pop_string {
