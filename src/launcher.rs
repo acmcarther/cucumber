58d41f8a6d3cedd3d94f0945e5f155c80b888a46
Patrick Junger <mail@pjunger.com>
2016-09-06 17:52:48 +0200

    Added msg to must_use

--- src/launcher.rs
+++ src/launcher.rs
@@ -9,7 +9,7 @@ use std::time::Duration;
 use std::env;
 
 #[derive(Default)]
-#[must_use]
+#[must_use = "This struct has to be consumed by start method!"]
 pub struct CucumberConfig<'a, W: Send + 'static> {
   world: W,
   addr: &'static str,
